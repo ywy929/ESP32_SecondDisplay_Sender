bccf126f09ea3dace5e4ca54353c672a3f8991ce
ywy929 <aquamagmayeow94@gmail.com>
2024-11-24 23:24:52 +0800

    Add AND round and rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@ int lastButtonState = LOW;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 // MAC Address of ESP32 Receiver
-uint8_t address[6] = {0xd8, 0xbc, 0x38, 0xfc, 0x5b, 0x44};
-//uint8_t addresstwo[6] = {0xcc, 0xdb, 0xa7, 0x3e, 0xb7, 0xf8};
+uint8_t address[6] = {0xcc, 0xdb, 0xa7, 0x3e, 0x01, 0x28};
+// uint8_t addresstwo[6] = {0xcc, 0xdb, 0xa7, 0x3e, 0xb7, 0xf8};
 
 typedef struct test_struct
 {
@@ -91,13 +91,15 @@ void showNewData()
     String trimmedCopy = String(receivedChars);
 
     trimmed.trim();
-    // Serial.println("Setting: " + String(digitalRead(MODEMT1_PIN))+ String(digitalRead(MODEMT2_PIN))+ String(digitalRead(MODEAND_PIN)));
-    if (trimmed.indexOf("C308720092") != -1)
+    //Serial.println("Setting: " + String(digitalRead(MODEMT1_PIN)) + String(digitalRead(MODEMT2_PIN)) + String(digitalRead(MODEAND_PIN)));
+    if (digitalRead(MODEMT1_PIN) == LOW)
+    {
+      if (trimmed.indexOf("C308720092") != -1)
       {
         newData = false;
         return;
       }
-      Serial.println(trimmed.length());
+      //Serial.println(trimmed.length());
       if (trimmed.length() != 16)
       {
         newData = false;
@@ -117,10 +119,141 @@ void showNewData()
         newData = false;
         return;
       }
+    }
+    else if (digitalRead(MODEMT2_PIN) == LOW)
+    {
+      if (trimmed.indexOf("C308720092") != -1)
+      {
+        newData = false;
+        return;
+      }
+      Serial.println(trimmed.length());
+      if (trimmed.length() != 14)
+      {
+        newData = false;
+        return;
+      }
+
+      if (trimmed.startsWith("SD"))
+      {
+        trimmed.replace("SD", "    ");
+      }
+      else if (trimmed.startsWith("S"))
+      {
+        trimmed.replace("S", " * ");
+      }
+      else
+      {
+        newData = false;
+        return;
+      }
+    }
+    else if (digitalRead(MODEAND_PIN) == LOW)
+    {
+      Serial.println(trimmed.length());
+      if (trimmed.length() != 15)
+      {
+        newData = false;
+        return;
+      }
+
+      if (trimmed.startsWith("ST,+0000"))
+      {
+        trimmed.replace("ST,+0000", " *        ");
+      }
+      else if (trimmed.startsWith("ST,+000"))
+      {
+        trimmed.replace("ST,+000", " *       ");
+      }
+      else if (trimmed.startsWith("ST,+00"))
+      {
+        trimmed.replace("ST,+00", " *      ");
+      }
+      else if (trimmed.startsWith("ST,+0"))
+      {
+        trimmed.replace("ST,+0", " *     ");
+      }
+      else if (trimmed.startsWith("ST,+"))
+      {
+        trimmed.replace("ST,+", " *    ");
+      }
+      else if (trimmed.startsWith("US,+0000"))
+      {
+        trimmed.replace("US,+0000", "          ");
+      }
+      else if (trimmed.startsWith("US,+000"))
+      {
+        trimmed.replace("US,+000", "         ");
+      }
+      else if (trimmed.startsWith("US,+00"))
+      {
+        trimmed.replace("US,+00", "        ");
+      }
+      else if (trimmed.startsWith("US,+0"))
+      {
+        trimmed.replace("US,+0", "       ");
+      }
+      else if (trimmed.startsWith("US,+"))
+      {
+        trimmed.replace("US,+", "      ");
+      }
+      else if (trimmed.startsWith("ST,-0000"))
+      {
+        trimmed.replace("ST,-0000", " *       -");
+      }
+      else if (trimmed.startsWith("ST,-000"))
+      {
+        trimmed.replace("ST,-000", " *      -");
+      }
+      else if (trimmed.startsWith("ST,-00"))
+      {
+        trimmed.replace("ST,-00", " *     -");
+      }
+      else if (trimmed.startsWith("ST,-0"))
+      {
+        trimmed.replace("ST,-0", " *    -");
+      }
+      else if (trimmed.startsWith("ST,-"))
+      {
+        trimmed.replace("ST,-", " *   -");
+      }
+      else if (trimmed.startsWith("US,-0000"))
+      {
+        trimmed.replace("US,-0000", "         -");
+      }
+      else if (trimmed.startsWith("US,-000"))
+      {
+        trimmed.replace("US,-000", "        -");
+      }
+      else if (trimmed.startsWith("US,-00"))
+      {
+        trimmed.replace("US,-00", "       -");
+      }
+      else if (trimmed.startsWith("US,-0"))
+      {
+        trimmed.replace("US,-0", "      -");
+      }
+      else if (trimmed.startsWith("US,-"))
+      {
+        trimmed.replace("US,-", "     -");
+      }
+      else
+      {
+        newData = false;
+        return;
+      }
+
+      if (trimmed.endsWith("  g"))
+      {
+        trimmed.replace("  g", " g");
+      }
+      trimmedCopy = trimmed;
+    }
 
     // Serial.println(digitalRead(BUTTON_PIN));
     //  convert to tael singapore (tls) if button is pressed
-    if (digitalRead(BUTTON_PIN) == HIGH)
+
+    if (digitalRead(BUTTON_PIN) == HIGH && digitalRead(MODEAND_PIN) == HIGH)
     {
       int stable = 1;
       if (trimmedCopy.startsWith("S S"))
@@ -147,12 +280,37 @@ void showNewData()
       }
       trimmed = tlsString;
     }
+    else if (digitalRead(BUTTON_PIN) == HIGH && digitalRead(MODEAND_PIN) == LOW)
+    {
+      int stable = 1;
+      if (trimmedCopy.startsWith(" *"))
+      {
+        stable = 1;
+        trimmedCopy.replace(" *","");
+      }else{
+        stable = 0;
+      }
+
+      trimmedCopy.replace("g", "");
+      trimmedCopy.trim();
+      float tlsFloat = trimmedCopy.toFloat();
+      tlsFloat = tlsFloat / 37.7994;
+      String tlsString = String(tlsFloat, 3);
+      tlsString = tlsString + " t";
+      tlsString = padLeft(tlsString, ' ', 16);
+      if (stable)
+      {
+        tlsString[1] = '*';
+        Serial.println(tlsString);
+      }
+      trimmed = tlsString;
+    }
     const char *data = trimmed.c_str();
     // uint8_t *data = (uint8_t *)trimmed.c_str();
     strcpy(myData.a, data);
     // Send message via ESP-NOW
     esp_err_t result = esp_now_send(address, (uint8_t *)&myData, sizeof(test_struct));
-    //esp_err_t result_two = esp_now_send(addresstwo, (uint8_t *)&myData, sizeof(test_struct));
+    // esp_err_t result_two = esp_now_send(addresstwo, (uint8_t *)&myData, sizeof(test_struct));
 
     newData = false;
   }
@@ -185,8 +343,8 @@ void setup()
     return;
   }
   // register second peer
-  //memcpy(peerInfo.peer_addr, addresstwo, 6);
-  //if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  // memcpy(peerInfo.peer_addr, addresstwo, 6);
+  // if (esp_now_add_peer(&peerInfo) != ESP_OK)
   //{
   //  Serial.println("Failed to add peer");
   //  return;
